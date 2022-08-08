88e58dee4fe0f778d6e3033f447a9ad991269982
zdoue <zdoue@10.0.0.198>
2022-08-07 22:04:32 -0400

    coomit

--- src/Demo/DemoTest.java
+++ src/Demo/DemoTest.java
@@ -11,4 +11,9 @@ public class DemoTest {
 	public static void main1(String[] args) {
 	  System.out.println("testing");
 }
+	public static void add(int a , int b) {
+		System.out.println(a+b);
+	} 
+	
 }
+
